89274a83015b4cf71753e7de05f401c82e408908
oopanpan <oopanpan@gmail.com>
2021-03-05 14:52:46 -0800

    log in built

--- ameowsment_backend_api/app/controllers/games_controller.rb
+++ ameowsment_backend_api/app/controllers/games_controller.rb
@@ -1,5 +1,10 @@
 class GamesController < ApplicationController
 
+    def index
+        games = Game.all
+        render json: games
+    end
+
     def show
         game = Game.find(params[:id])
         render json: game
--- ameowsment_frontend/main.js
+++ ameowsment_frontend/main.js
@@ -1,34 +1,87 @@
+const usersURL = "http://localhost:3000/users"
+const gamesURL = "http://localhost:3000/games"
+const main = document.querySelector('div#main-container')
+let currentUser
 
-//* THINGGY MOVING AROUND MODULES
-let div = document.querySelector('div#testing')
+console.log('H1')
+console.log(document.readyState)
 
-let setXProperty = (xLocation, yLocation) => {
-    div.style.setProperty('--xLocation', xLocation + '%')
-    div.style.setProperty('--yLocation', yLocation + '%')
-}
+document.addEventListener('DOMContentLoaded',()=>{
 
-let changeLocation = () =>{
-    const xLocation = Math.random()*100;
-    console.log(xLocation)
-    const yLocation = Math.random()*100;
-    setXProperty(xLocation, yLocation);
-}
+    const userLogin = () =>{
+        const loginDiv = document.createElement('div')
+        const loginForm = document.createElement('form')
+        const userInput = document.createElement('input')
+        const submitBtn = document.createElement('button')
+        loginDiv.id = 'login-container'
+        userInput.type = 'text'
+        userInput.name = 'username'
+        userInput.placeholder = 'Enter Username here'
+        submitBtn.innerText = 'Login'
+        loginForm.addEventListener('submit', submitHandle )
+        loginForm.append(userInput, submitBtn)
+        loginDiv.appendChild(loginForm)
+        main.appendChild(loginForm)
+        
+    }
 
-setInterval(changeLocation, 1200);
+    const submitHandle = (e) =>{
+        e.preventDefault()
+        let userName = {
+            username: e.target[0].value
+        }
+        axios.post(usersURL, userName)
+        .then(r => {
+            currentUser = r.data
+            loadGameMenu()
+        })
+    }
 
-//* LV2 MODULES
-let div2 = document.querySelector('div#testing2')
+    const loadGameMenu = () =>{
+        axios.get(gamesURL)
+        .then(console.log)
+    }
 
-let setYProperty = (xLocation, yLocation) => {
-    div2.style.setProperty('--xLocation', xLocation + '%')
-    div2.style.setProperty('--yLocation', yLocation + '%')
-}
+    const redDotGame = () =>{
+        // const 
+        // const targetDiv = document.createElement('div')
+        // const 
+    }
 
-let changeAnotherLocation = () =>{
-    const xLocation = Math.random()*100;
-    console.log(xLocation)
-    const yLocation = Math.random()*100;
-    setYProperty(xLocation, yLocation);
-}
+    userLogin()
+})
 
-setInterval(changeAnotherLocation, 1500)
+
+// //* THINGGY MOVING AROUND MODULES
+// let div = document.querySelector('div#testing')
+
+// let setXProperty = (xLocation, yLocation) => {
+//     div.style.setProperty('--xLocation', xLocation + '%')
+//     div.style.setProperty('--yLocation', yLocation + '%')
+// }
+
+// let changeLocation = () =>{
+//     const xLocation = Math.random()*100;
+//     console.log(xLocation)
+//     const yLocation = Math.random()*100;
+//     setXProperty(xLocation, yLocation);
+// }
+
+// setInterval(changeLocation, 1200);
+
+// //* LV2 MODULES
+// let div2 = document.querySelector('div#testing2')
+
+// let setYProperty = (xLocation, yLocation) => {
+//     div2.style.setProperty('--xLocation', xLocation + '%')
+//     div2.style.setProperty('--yLocation', yLocation + '%')
+// }
+
+// let changeAnotherLocation = () =>{
+//     const xLocation = Math.random()*100;
+//     console.log(xLocation)
+//     const yLocation = Math.random()*100;
+//     setYProperty(xLocation, yLocation);
+// }
+
+// setInterval(changeAnotherLocation, 1500)
